1f31d95f39fd4a470b2f01152b5ef84a5242afc1
toronto966 <toronto9696@gmail.com>
2017-12-19 01:50:58 +0700

    hey hey

--- BaiTap/bai2/js-practices/Practice1/searchPractice.js
+++ BaiTap/bai2/js-practices/Practice1/searchPractice.js
@@ -1,8 +1,7 @@
 'use strict'
 
 function search(input, target) {
-  // return input.indexOf(target);  // Remove this line and change to your own algorithm
-   if(input.length > 1){
+
     for(var i = 0; i < input.length; i++){
       if(input[i] == target){
           return i;
@@ -10,9 +9,7 @@ function search(input, target) {
           continue;
       }
     } 
-  }else if(input.length <= 1){
-    return -1;
-  }
+  
 
   return -1;
 }

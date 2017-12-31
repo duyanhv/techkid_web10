6fb48372681003a327031f536dc484e5fbefe3c2
toronto966 <toronto9696@gmail.com>
2018-01-01 01:00:16 +0700

    hey

--- web10_buoi4/public/mainpage/script.js
+++ web10_buoi4/public/mainpage/script.js
@@ -1,3 +1,22 @@
 document.querySelector("#nav_toggle").addEventListener( "click", function() {
   this.classList.toggle("active");
 });
+
+function whyTho(){
+  alert("There's a freaking button over there, click it!");
+}
+
+let flag = true;
+function navButton(){    
+  
+  if(flag){
+      document.getElementById("overlay_id").style.width = "100%";
+      flag = false;
+      console.log("open");
+    }else{
+      document.getElementById("overlay_id").style.width = "0%";
+      flag = true;
+      console.log("close");
+    }
+  }
+

5a5a98c9dcfcb3f27fde91154a440eb10eef6e3d
toronto966 <toronto9696@gmail.com>
2018-01-09 07:42:28 +0700

    hey hey

--- web10_buoi4/public/test/ask.js
+++ web10_buoi4/public/test/ask.js
@@ -4,10 +4,20 @@ function reloadSite(){
 var i = 200;
 $(document).ready(function(){
     $("#num_count").text(200);
-    $("textarea").keypress(function(){
-        $("#num_count").text(i -= 1);
-        if(i == 0){
-            $("textarea").prop('disabled', true);
+    
+
+    $('textarea').keydown(function(){
+        // $("#num_count").text(i -= 1);
+        // if(i == 0){
+        //     $("textarea").prop('disabled', true);
+        // }
+        var numTa = $(this).val().length;
+        $("#num_count").text(200 - numTa);
+        // console.log(currentCharTa);
+        if(numTa > 200){
+            console.log(numTa);
+            $("#num_count").text(0);
+            $(this).prop('disabled', true);
         }
     });
     
